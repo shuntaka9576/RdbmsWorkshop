7f256cdc9f25957b58c54eaa194477c6bd00d631
shuntaka9576 <shuntaka9576@gmail.com>
2019-03-16 16:59:01 +0900

    Refactoring

--- indexworkshop/sql/start.sh
+++ indexworkshop/sql/start.sh
@@ -10,25 +10,25 @@ MYSQL_ENDPOINT=127.0.0.1
 testQuery() {
   log '======== start query ========'
   log 'show index'
-  mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source ./sql/showIndex.sql";
+  mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source showIndex.sql";
   log 'explain'
-  mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source ./sql/explainQuery.sql";
-  time (cat ./sql/testQuery.sql | mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd > /dev/null)
+  mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source explainQuery.sql";
+  time (cat testQuery.sql | mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd > /dev/null)
   log '======== end query ========'
 }
 
 log 'Create dabase and table data'
-mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source ./sql/createData.sql;show warnings;commit;";
+mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source createData.sql;show warnings;commit;";
 
 log '*=*=*=*=*=*=*=*=*=*=*=*=*=*=*= Run test query(non index) *=*=*=*=*=*=*=*=*=*=*=*=*=*=*='
 testQuery
 
 log 'Add index(use index)'
-mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source ./sql/addIndex.sql;show warnings;commit;";
+mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source addIndex.sql;show warnings;commit;";
 log '*=*=*=*=*=*=*=*=*=*=*=*=*=*=*= Run test query(use index) *=*=*=*=*=*=*=*=*=*=*=*=*=*=*='
 testQuery
 
 log 'Add index(use index_fast)'
-mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source ./sql/addIndex_fast.sql;show warnings;commit;";
+mysql -h $MYSQL_ENDPOINT -u root -pPassw0rd -t -e "source addIndex_fast.sql;show warnings;commit;";
 log '*=*=*=*=*=*=*=*=*=*=*=*=*=*=*= Run test query(use index_fast) *=*=*=*=*=*=*=*=*=*=*=*=*=*=*='
 testQuery
